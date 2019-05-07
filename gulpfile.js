d46778222c3ad6815d12f829364e233af958835c
tylermachado <3586466+tylermachado@users.noreply.github.com>
2019-05-07 13:36:11 -0400

    giving up on previous gulp/CSV library

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ var cache = require('gulp-cache');
 var strip = require('gulp-strip-comments');
 var htmlbeautify = require('gulp-html-beautify');
 var autoprefixer = require('gulp-autoprefixer');
-var csv2json = require('gulp-csv-to-json');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -79,12 +78,16 @@ gulp.task('images', function(){
 
 /* see above note about images, same applies here */
 gulp.task('csvToJson', function(){
+    var options = {};
   return gulp.src('app/data/**/*.csv')
-  .pipe(csv2json())
+  // .pipe(csv2json(options))
+  // .pipe(uglify().on('error', function(e){
+  //           console.log(e);
+  //        }))
   .pipe(gulp.dest('dist/interactive/2018/10/bubble/data'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  // .pipe(browserSync.reload({
+  //   stream: true
+  // }))
   .pipe( notify({ message: "CSVs have been converted to JSON and moved!"}) );
 });
 

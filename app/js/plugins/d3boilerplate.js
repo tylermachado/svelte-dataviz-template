0f3628bd3c768e69f4cb0fdda992aa78a268b2fe
tylermachado <3586466+tylermachado@users.noreply.github.com>
2019-05-09 11:16:45 -0400

    starting D3 boilerplate file

---
+++ app/js/plugins/d3boilerplate.js
@@ -0,0 +1,9 @@
+// The svg
+var container = d3.select(".wrapper"),
+    width = window.innerWidth,
+    height = window.innerWidth * 0.5;
+
+// var colorsGeneral = ["#d51e2d", "#05aec0", "#6f2b6e", "#71bd69", "#1c35b7", "#b3e61c"];
+var colorsGeneral = ["#d51e2d", "#52CFE5", "#385775", "#FFBF3D", "#6f2b6e", "#00CFB5"];
+var colorsProcon = ["#ce0201", "#a5d65a"];
+var colorsPolitical = ["#D41B2C", "#006EB5"];

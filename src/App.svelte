51200c729459ba1debde79fced6349e3d411245b
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-05-20 11:48:22 -0400

    test chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+
+	let el;
+
+	onMount(() => {
+		d3.csv("/datasets/popdensity.csv")
+		.then(function(data) {
+			data.sort(function (a, b) {
+			  return b["Density"] - a["Density"];
+			});
+
+			d3.select(el)
+				.selectAll("div")
+				.data(data)
+				.enter()
+				.append("div")
+				.style("width", function(d) {
+					return (+d["Density"] * 30) + "px";
+				})
+				.text(function(d) {
+					return d["City"];
+				});
+		});
+	});
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
 	}
 </style>
+
+<div bind:this={el} class="chart"></div>

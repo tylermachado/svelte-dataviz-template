3952a7c8dd375186fce9c6ca57306fef206c771a
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-05-27 14:00:56 -0400

    early attempt at hover tooltip

---
+++ src/HoverCard.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+</script>
+
+<style>
+	#map-tooltip {
+		background-color:white;
+		border:1px solid black;
+		border-radius: 4px;
+		padding:10px;
+		display:inline-block;
+	}
+
+	h4 {
+		font-size:1.2rem;
+		margin:0;
+	}
+
+	div.infocell {
+		display:inline-block;
+		width:100px;
+		font-size:0.7rem;
+	}
+
+	span.value {
+		font-size:1.3rem;
+		font-weight: 700;
+		color: darkgreen;
+		display:block;
+	}
+
+
+</style>
+
+<div id="map-tooltip">
+	<h4>Boston, MA</h4>
+	<div class="infocell">City Population: <span class="value">674913</span></div>
+	<div class="infocell">Acres of Parkland: <span class="value">5072</span></div>
+	<div class="infocell">Percent of Residents within Half-Mile Walk of Park: <span class="value">100%</span></div>
+</div>
--- src/MapUSA.svelte
+++ src/MapUSA.svelte
@@ -4,18 +4,36 @@
    import { onMount } from "svelte";
    import { feature } from "topojson";
    import { join } from "./join.js";
+   import HoverCard from "./HoverCard.svelte"
+    import SvelteTooltip from 'svelte-tooltip';
 
    export let citylist;
    let data;
 
    const projection = geoAlbersUsa();
    const path = geoPath().projection(projection);
-   let fillscale = d3.scaleLog()
-   .range([5, 50]);
-   let slice = "population"
+
+   let radiusScale = d3.scaleLog()
+   .range([2, 50]);
+   let radiusVariable = "population"
+
+   let colorScale = d3.scaleSequential()
+      .interpolator(d3.interpolateBuGn);
+   let colorVariable = "walkablepct"
 
    function handleMouseover(city, event) {
-      // console.log(event)
+      console.log(event.clientY)
+
+      d3.select('#map-tooltip')
+         .style('display',    'block')
+         .style('position',   'absolute')
+         .style('top',        (event.clientY + 10) + "px")
+         .style('left',       (event.clientX - 150) + "px")
+   }
+
+   function handleMouseout(city, event) {
+      d3.select('#map-tooltip')
+         .style('display',    'none')
    }
 
 
@@ -34,27 +52,44 @@
       d3.csv("datasets/parksdata.csv")
       ])
       .then(function(data) {
-         var firstresult = join(data[1], data[0], "City", "city", function(cityloc, dens) {
-            return {
-               city: cityloc.city,
-               lat: +cityloc.lat,
-               lon: +cityloc.lon,
-               population: parseFloat((dens["City Population"]).replace(/,/g, '')),
-               landarea: parseFloat((dens["Land Area"]).replace(/,/g, '')),
-               revisedarea: parseFloat((dens["Revised Area"]).replace(/,/g, '')),
-               density: +dens["Density"],
-               walkablepct: isNaN(parseFloat((dens["Percent of Residents within Half-Mile Walk of Park"]).replace(/%/g, ''))),
-               acres: +dens["Acres of Parkland"],
-               acresper1000: +dens["Parkland per 1,000 Residents"]
-            };
+         var combodata = join(data[1], data[0], "City", "city", function(cityloc, dens) {
+            let walkablepct = parseFloat((dens["Percent of Residents within Half-Mile Walk of Park"]).replace(/%/g, ''));
+
+               return {
+                  city: cityloc.city,
+                  lat: +cityloc.lat,
+                  lon: +cityloc.lon,
+                  population: parseFloat((dens["City Population"]).replace(/,/g, '')),
+                  landarea: parseFloat((dens["Land Area"]).replace(/,/g, '')),
+                  revisedarea: parseFloat((dens["Revised Area"]).replace(/,/g, '')),
+                  density: +dens["Density"],
+                  walkablepct: parseFloat((dens["Percent of Residents within Half-Mile Walk of Park"]).replace(/%/g, '')),
+                  acres: +dens["Acres of Parkland"],
+                  acresper1000: +dens["Parkland per 1,000 Residents"]
+               }
+
          });
 
-         fillscale.domain(
-         [Math.min.apply(Math, firstresult.map(function(o) { return o[slice]; })),
-         Math.max.apply(Math, firstresult.map(function(o) { return o[slice]; }))]
+         combodata.sort(function (a, b) {
+           return b.population - a.population;
+         });
+
+         if (colorVariable == "walkablepct") {
+            combodata = combodata.filter(d => {
+               return !isNaN(d.walkablepct);
+            })
+         }
+         radiusScale.domain(
+            [Math.min.apply(Math, combodata.map(function(o) { return o[radiusVariable]; })),
+            Math.max.apply(Math, combodata.map(function(o) { return o[radiusVariable]; }))]
          )
 
-         return firstresult;
+         colorScale.domain(
+            [Math.min.apply(Math, combodata.map(function(o) { return o[colorVariable]; })),
+            Math.max.apply(Math, combodata.map(function(o) { return o[colorVariable]; }))]
+         )
+
+         return combodata;
       });
 
       citylist = await files;
@@ -78,16 +113,19 @@
    <path d={data} class="border" />
    {#if citylist}
    {#each citylist as city}
-   <circle
-   class="city"
-   cx={projection([city.lon, city.lat])[0]}
-   cy={projection([city.lon, city.lat])[1]}
-   r={fillscale(city[slice])}
-   opacity="0.5"
-   fill="#888888"
-   stroke="#000000"
-   on:mouseover={handleMouseover(city.city, event)}
-   />
+      <circle
+         class="city"
+         cx={projection([city.lon, city.lat])[0]}
+         cy={projection([city.lon, city.lat])[1]}
+         r={radiusScale(city[radiusVariable])}
+         opacity="0.8"
+         fill={colorScale(city[colorVariable])}
+         stroke="#000000"
+         on:mousemove={handleMouseover(city.city, event)}
+         on:mouseout={handleMouseout(city.city, event)}
+      />
    {/each}
    {/if}
 </svg>
+
+<HoverCard />
